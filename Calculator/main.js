a96f1e4f33215530773e1c16af6beefd6c8bc8f9
KuangjuX <18630816527@163.com>
2020-06-08 21:26:19 +0800

    Calculator

---
+++ Calculator/main.js
@@ -0,0 +1,11 @@
+
+let string="";//全局变量
+
+function display() {
+    let accumulator=document.querySelector('div.accumulator');
+    let value=this.innerText;
+    alert(value);
+    /*string+=value;
+    accumulator.innerHTML=string;*/
+
+}
