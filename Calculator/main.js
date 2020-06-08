7b3ce99fd7bf281c80fcee3c5cf668eb3f71e9ea
KuangjuX <18630816527@163.com>
2020-06-08 23:19:40 +0800

    Calculator

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,11 +1,19 @@
 
 let string="";//全局变量
 
-function display() {
+function display(event) {
     let accumulator=document.querySelector('div.accumulator');
-    let value=this.innerText;
-    alert(value);
-    /*string+=value;
-    accumulator.innerHTML=string;*/
+    let value=event.target.innerText;
+    string+=value;
+    accumulator.innerHTML=string;
+}
 
+
+//按=进行运算
+function count() {
+    
+}
+//按CE删除所有数字
+function remove() {
+    string="";
 }
