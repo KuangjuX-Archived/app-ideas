eaf0c03fd350e204ee05a0b17ffcb10153d56ec1
KuangjuX <18630816527@163.com>
2020-06-08 19:45:21 +0800

    Bin2Dec

--- Bin2Dec/main.js
+++ Bin2Dec/main.js
@@ -2,7 +2,7 @@ function Count(BinaryNumber) {
     let length=BinaryNumber.length;
     let sum=0;
     for(let i=length-1;i>=0;i--){
-        if(Number(BinaryNumber[i])!==0||Number(BinaryNumber[i])!==1){
+        if(Number(BinaryNumber[i])!==0&&Number(BinaryNumber[i])!==1){
             alert("Please enter a correct binary number.");
             return 0;
         }
