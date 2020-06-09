a78bbfc7eddbbe24dc46e9f0711d5475f0ecf93e
KuangjuX <18630816527@163.com>
2020-06-09 10:37:17 +0800

    Calculator

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,19 +1,66 @@
+let string="";//全局字符串
 
-let string="";//全局变量
+let value=0;//全局计算结果
+let timestamp=0;//时间戳，记录按运算符数量
 
-function display(event) {
+function display(number) {
     let accumulator=document.querySelector('div.accumulator');
-    let value=event.target.innerText;
-    string+=value;
+    string+=number;
     accumulator.innerHTML=string;
 }
 
+function displayResult() {
+    if(timestamp>=2){
+        let i=0;
+        for(i;i<string.length-1;i++){
+            if(string[i]==='+'||string[i]==='-'||string[i]==='x'||string[i]==='/'||string[i]==='%'){
+                break;
+            }
+        }
+        let num1=Number(string.slice(0,i));
+        let num2=Number(string.slice(i+1,string.length));
+        let result=0;
+        switch (string[i]) {
+            case '+':
+                result=num1+num2;
+                break;
+            case '-':
+                result=num1-num2;
+                break;
+            case 'x':
+                result=num1*num2;
+                break;
+            case '/':
+                result=num1/num2;
+                break;
+            case '%':
+                result=num1%num2;
+                break;
+        }
 
-//按=进行运算
-function count() {
-    
+        document.querySelector('div.total').innerHTML=String(result);
+        string=String(result);
+        timestamp--;
+    }
+}
+//按运算符进行运算
+function count(operator) {
+    /*string+=operator;*/
+    if(operator==='='){
+        timestamp++;
+        displayResult();
+        timestamp=0;
+        return;
+    }
+    timestamp++;
+    displayResult();
+    string+=operator;
 }
 //按CE删除所有数字
 function remove() {
     string="";
+    document.querySelector('div.accumulator').innerHTML=string;
+    document.querySelector('div.total').innerHTML="";
+    timestamp=0;
+
 }
--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 This is a repository to finish the project of [app-ideas](https://github.com/florinpop17/app-ideas).
 
-- [Bin2Dec](/Bin2Dec)
+[**Bin2Dec**](/Bin2Dec):
+
+This is a converter that converts binary numbers to decimal numbers.
+
+![](images/Bin2Dec.png)
+
+[**Calculator**](/Calculator):
+
+This is a simple calculator.
+
+![](images/Calculator.png)
